20e4d91493c792f033eb6f66e4068652a32ba236
Namito Yokota <namitoyokota@icloud.com>
2023-10-15 21:34:49 -0500

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+    /** Indicates root folder with full images */
+    readonly defaultFilePath = 'assets/images/';
+
+    /** Indicates root folder with compressed images */
+    readonly compressedFilePath = 'assets/images-compressed/';
+
+    /** Name of each image file */
+    readonly photoFileNames = [
+        'JAN_6166',
+        'JAN_5989',
+        'JAN_5978',
+        'JAN_5869',
+        'JAN_3375',
+        'JAN_3144',
+        'JAN_2711',
+        'JAN_2704',
+        'JAN_2643',
+        'JAN_2574',
+        'JAN_2214',
+        'JAN_2113',
+    ];
+
+    /** Extension used to display each image file */
+    readonly extension = '.jpg';
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { LazyLoadImageModule } from 'ng-lazyload-image';
+import { AppComponent } from './app.component';
+
+@NgModule({
+    declarations: [AppComponent],
+    imports: [BrowserModule, LazyLoadImageModule],
+    providers: [],
+    bootstrap: [AppComponent],
+})
+export class AppModule {}
