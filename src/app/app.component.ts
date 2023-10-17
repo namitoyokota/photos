19fbb61dceba4da9d4fce107deb7393b324ae557
Namito Yokota <namitoyokota@icloud.com>
2023-10-16 21:26:12 -0500

    new pictures with alt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { KeyValue } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,6 +7,11 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+    /** Used to sort map in HTML by descending order */
+    descOrder = (a: KeyValue<string, string>, b: KeyValue<string, string>): number => {
+        return a.key > b.key ? -1 : b.key > a.key ? 1 : 0;
+    };
+
     /** Indicates root folder with full images */
     readonly defaultFilePath = 'assets/images/';
 
@@ -13,23 +19,44 @@ export class AppComponent {
     readonly compressedFilePath = 'assets/images-compressed/';
 
     /** Name of each image file */
-    readonly photoFileNames = [
-        'JAN_6166',
-        'JAN_5989',
-        'JAN_5978',
-        'JAN_5869',
-        'JAN_3375',
-        'JAN_3144',
-        'JAN_2711',
-        'JAN_2704',
-        'JAN_2643',
-        'JAN_2574',
-        'JAN_2214',
-        'JAN_2113',
-    ];
+    readonly photoFileNames = new Map<string, string>([
+        ['JAN_6166', 'condo - bell'],
+        ['JAN_5989', 'condo - girls'],
+        ['JAN_5978', 'condo - girls'],
+        ['JAN_5869', 'hiking - bell'],
+        ['JAN_3375', 'beach - girls'],
+        ['JAN_3144', 'beach - bell'],
+        ['JAN_3042', 'beach - chloe'],
+        ['JAN_3564', 'beach - chloe'],
+        ['JAN_2711', 'beach walk - lifeguard'],
+        ['JAN_2704', 'beach walk - lemonade'],
+        ['JAN_2621', 'beach walk - girls'],
+        ['JAN_2643', 'beach walk - girls'],
+        ['JAN_2572', 'beach walk - lemonade'],
+        ['JAN_2214', 'beach condo - girls'],
+        ['JAN_2113', 'beach condo - girls'],
+        ['JAN_1649', 'denny chimes - hannah and haylee'],
+        ['JAN_1817', 'bryant denny - haylee'],
+        ['JAN_1849', 'graduation - hannah'],
+        ['JAN_1853', 'graduation - hannah'],
+        ['JAN_1887', 'graduation - hannah'],
+        ['JAN_1894', 'graduation - haylee'],
+        ['JAN_1267', 'gulf coast - speaker'],
+        ['JAN_1986', 'gulf coast - worship'],
+        ['JAN_2067', 'gulf coast - worship'],
+        ['JAN_3726', 'horse - bell'],
+        ['JAN_3736', 'horse - bell'],
+        ['JAN_3852', 'horse - anna'],
+        ['JAN_4137', 'hiking - braden'],
+        // ['JAN_1807', 'bryant denny - hannah and haylee']
+        // ['JAN_1811', 'bryant denny - haylee']
+        // ['JAN_2740' 'beach chair'],
+    ]);
 
     /** Extension used to display each image file */
     readonly extension = '.jpg';
 
-    constructor() {}
+    constructor() {
+        console.log(this.photoFileNames);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CommonModule } from '@angular/common';
 import { LazyLoadImageModule } from 'ng-lazyload-image';
 import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, LazyLoadImageModule],
+    imports: [BrowserModule, LazyLoadImageModule, CommonModule],
     providers: [],
     bootstrap: [AppComponent],
 })
